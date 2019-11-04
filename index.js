9ce2960088e93cfb0a876c2e7068125506864f20
Veer_Davinder <birdevinder1705@gmail.com>
2019-11-04 17:14:19 -0500

    Laptop blog

--- index.js
+++ index.js
@@ -6,15 +6,42 @@ const laptopData = JSON.parse(json);
 
 const server = http.createServer((req,res)=>{
     const pathName= url.parse(req.url,true).pathname;
+    const id = url.parse(req.url,true).query.id;
+    
 
+    //Product Overview
     if(pathName === '/products' || pathName === '' ){
     res.writeHead(200,{'Content-type':'text/html'});
-    res.end('This is the Product Page');
+    
+    res.writeHead(200,{'Content-type':'text/html'});
+    fs.readFile(`${__dirname}/overview.html`,'utf-8',(err,data)=>{
+        let overviewOutput = data;
+        fs.readFile(`${__dirname}/card.html`,'utf-8',(err,data)=>{
+            const cardsOutput = laptopData.map(el => replaceFun(data,el)).join('');
+            overviewOutput = overviewOutput.replace('{%CARDS%}',cardsOutput);
+            res.end(overviewOutput);
+        });
+    });
     }
-    else if(pathName === '/laptop'){
+
+    //Laptop Detail
+    else if(pathName === '/laptop' && id <laptopData.length){
         res.writeHead(200,{'Content-type':'text/html'});
-        res.end('This is the Laptop Page');
+        fs.readFile(`${__dirname}/laptop.html`,'utf-8',(err,data)=>{
+            const laptop = laptopData[id];
+            const output = replaceFun(data,laptop); 
+            res.end(output)
+        })
     }
+
+    //IMAGES 
+    else if ((/\.(jpg|jpeg|png|gif)$/i).test(pathName)) {
+        fs.readFile(`${__dirname}/data/img${pathName}`, (err, data) => {
+            res.writeHead(200, { 'Content-type': 'image/jpg'});
+            res.end(data);
+        });
+    }
+    //URL not found
     else{
         res.writeHead(404,{'Content-type':'text/html'});
         res.end('URL was not found');
@@ -25,3 +52,16 @@ const server = http.createServer((req,res)=>{
 server.listen(1337,'127.0.0.1',()=>{
     console.log('Listening for requests now')
 })
+
+function replaceFun(originalHtml,laptop){
+    let output = originalHtml.replace(/{%PRODUCTNAME%}/g,laptop.productName);
+    output = output.replace(/{%IMAGE%}/g, laptop.image);
+    output = output.replace(/{%PRICE%}/g,laptop.price);
+    output = output.replace(/{%SCREEN%}/g,laptop.screen);
+    output = output.replace(/{%CPU%}/g,laptop.cpu);
+    output = output.replace(/{%STORAGE%}/g,laptop.storage);
+    output = output.replace(/{%RAM%}/g,laptop.ram);
+    output = output.replace(/{%DESCRIPTION%}/g,laptop.description);
+    output = output.replace(/{%ID%}/g,laptop.id);
+    return output;
+}
